0a53919305d31b170f46e94d92f5c81c053179fb
ileis <gabrielileis07@gmail.com>
2026-07-01 16:36:56 -0300

    feat: implementacao da grid e highlight da grid

---
+++ Assets/Scripts/GridInputHandler.cs
@@ -0,0 +1,82 @@
+using System.Numerics;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GridInputHandler : MonoBehaviour
+{
+    public Camera mainCamera;
+    public LayerMask boardLayer;
+    public GameObject highlightObject;
+    public UnityEvent<Vector2Int> OnCellClicked;
+    public UnityEvent<Vector2Int> OnCellHovered;
+    
+    private Vector2Int lastHoveredCell = new Vector2Int(-1, -1);
+
+    private void Awake()
+    {
+        if (mainCamera == null) mainCamera = Camera.main;
+        if (highlightObject != null) highlightObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        HandleHover();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            HandleClick();
+        }
+    }
+
+    private bool TryGetGridCellUnderMouse(out Vector2Int gridPos)
+    {
+        gridPos = default;
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, 500f, boardLayer))
+        {
+            gridPos = GridManager.Instance.WorldToGrid(hit.point);
+            return GridManager.Instance.IsValidGridPosition(gridPos);
+        }
+        return false;
+    }
+
+    private void HandleHover()
+    {
+        if (TryGetGridCellUnderMouse(out Vector2Int gridPos))
+        {
+            if (gridPos != lastHoveredCell)
+            {
+                lastHoveredCell = gridPos;
+
+                if (highlightObject != null)
+                {
+                    highlightObject.SetActive(true);
+                    highlightObject.transform.position = GridManager.Instance.GridToWorld(gridPos) + UnityEngine.Vector3.up * 0.01f;
+                }
+
+                OnCellHovered?.Invoke(gridPos);
+            }
+        }
+        else
+        {
+            lastHoveredCell = new Vector2Int(-1, -1);
+            if (highlightObject != null) highlightObject.SetActive(false);
+        }
+    }
+
+    private void HandleClick()
+    {
+        if (TryGetGridCellUnderMouse(out Vector2Int gridPos))
+        {
+            OnCellClicked?.Invoke(gridPos);
+            Debug.Log($"Célula clicada: {gridPos}");
+        }
+    }
+}
---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,107 @@
+using System.Runtime.ExceptionServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public static GridManager Instance { get; private set; }
+
+    public int width = 10;
+    public int height = 10;
+    public float cellSize = 1f;
+    public Vector3 origin = new Vector3(-5f, 0f, -5f);
+    private GridCell[,] grid;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        grid = new GridCell[width, height];
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                grid[x, y] = new GridCell(x, y);
+            }
+        }
+    }
+
+    public Vector2Int WorldToGrid(Vector3 worldPosition)
+    {
+        int x = Mathf.FloorToInt((worldPosition.x - origin.x) / cellSize);
+        int y = Mathf.FloorToInt((worldPosition.z - origin.z) / cellSize);
+        return new Vector2Int(x, y);
+    }
+
+    public Vector3 GridToWorld(Vector2Int gridPosition)
+    {
+        float worldX = origin.x + gridPosition.x * cellSize + cellSize * 0.5f;
+        float worldZ = origin.z + gridPosition.y * cellSize + cellSize * 0.5f;
+        return new Vector3(worldX, origin.y, worldZ);
+    }
+
+    public bool IsValidGridPosition(Vector2Int gridPosition)
+    {
+        return gridPosition.x >= 0 && gridPosition.x < width
+            && gridPosition.y >= 0 && gridPosition.y < height;
+    }
+
+    public GridCell GetCell(Vector2Int gridPosition)
+    {
+        if (!IsValidGridPosition(gridPosition)) return null;
+        return grid[gridPosition.x, gridPosition.y];
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for (int x = 0; x <= width; x++)
+        {
+            Vector3 from = origin + new Vector3(x * cellSize, 0.01f, 0);
+            Vector3  to = origin + new Vector3(x * cellSize, 0.01f, height * cellSize);
+            Gizmos.DrawLine(from, to);
+        }
+
+        for (int y = 0; y <= height; y++)
+        {
+            Vector3 from = origin + new Vector3(0, 0.01f, y * cellSize);
+            Vector3 to = origin + new Vector3(width * cellSize, 0.01f, y * cellSize);
+            Gizmos.DrawLine(from, to);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+public class GridCell
+{
+    public int x;
+    public int y;
+    public bool isWalkable = true;
+    public bool isOccupied = false;
+
+    public object occupant = null;
+
+    public GridCell(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
